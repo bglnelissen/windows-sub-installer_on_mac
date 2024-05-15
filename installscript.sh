b16ebcbf75c250dd96212b7a9ff78a88cab3d324
Bastiaan Nelissen <bglnelissen@users.noreply.github.com>
2024-05-15 19:36:19 +0200

    Create installscript.sh

---
+++ installscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+brew install wimlib 
+diskutil list
+diskutil eraseDisk MS-DOS "WIN10" GPT /dev/disk4
+hdiutil mount ./Win10_22H2_EnglishInternational_x64v1.iso
+rsync -vha --exclude=sources/install.wim "/Volumes/CCCOMA_X64FRE_EN-GB_DV9" /Volumes/WIN10 
+wimlib-imagex split "/Volumes/CCCOMA_X64FRE_EN-GB_DV9/sources/install.wim" /Volumes/WIN10/sources/install.swm 4000
+diskutil eject /dev/disk4
+diskutil eject /dev/disk14
