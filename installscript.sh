ddf77099d75c30013380f7497a3980d58577eba0
Bastiaan Nelissen <bglnelissen@users.noreply.github.com>
2024-05-15 19:44:18 +0200

    Update installscript.sh
    forgot trailing slash

--- installscript.sh
+++ installscript.sh
@@ -4,7 +4,7 @@ brew install wimlib
 diskutil list
 diskutil eraseDisk MS-DOS "WIN10" GPT /dev/disk4
 hdiutil mount ./Win10_22H2_EnglishInternational_x64v1.iso
-rsync -vha --exclude=sources/install.wim "/Volumes/CCCOMA_X64FRE_EN-GB_DV9" /Volumes/WIN10 
+rsync -vha --exclude=sources/install.wim "/Volumes/CCCOMA_X64FRE_EN-GB_DV9/" /Volumes/WIN10 
 wimlib-imagex split "/Volumes/CCCOMA_X64FRE_EN-GB_DV9/sources/install.wim" /Volumes/WIN10/sources/install.swm 4000
 diskutil eject /dev/disk4
 diskutil eject /dev/disk14
